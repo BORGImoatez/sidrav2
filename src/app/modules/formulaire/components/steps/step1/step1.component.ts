e113abb35e8a50db8343979c2107dc5a74d871d7
Moatez BORGI <moatez@delomid-it.com>
2025-07-06 14:25:18 +0100

    Fix FormulaireData import path

--- src/app/modules/formulaire/components/steps/step1/step1.component.ts
+++ src/app/modules/formulaire/components/steps/step1/step1.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnInit, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { FormulaireData } from '../../../models/formulaire.model';
+import { FormulaireData } from '../../models/formulaire.model';
 
 @Component({
   selector: 'app-step1',
